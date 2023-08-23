a3fc64b64931de0a04bb1e45058ca7d8eeb6524d
benjaminbritton <b.britton@imperial.ac.uk>
2023-08-23 15:27:42 -0700

    improved loader example file with some descriptions

--- LoadData_example.m
+++ LoadData_example.m
@@ -1,13 +1,21 @@
+% Example use of the h5oinA loading script
+
+%folder that contains the h5oina files, 
+% e.g. fname_folder='C:\Users\benja\OneDrive\Documents\MATLAB\h5oina';
 fname_folder='C:\Users\benja\OneDrive\Documents\MATLAB\h5oina';
+
+%h5oina file, excluding final file extension & folder
+%if the '-PatternMatched' version exists, the script will also load that data
+% e.g. fname_slice_data='Mg 1 Specimen 1 SLICE_0156 SLICE_0156 161';
 fname_slice_data='Mg 1 Specimen 1 SLICE_0156 SLICE_0156 161';
 
+%location of MTEX 5.8.1
+%if MTEX loaded this doesn't matter
+%if MTEX is not loaded, then it will start MTEX up
 mtex_location='C:\Users\benja\OneDrive\Documents\MATLAB\mtex-5.8.1'; %working with 5.8.1
 
 [ebsd_original,dataset_header,ebsd_patternmatched] = load_h5oina_pm(fname_slice_data,fname_folder,mtex_location);
 
-% optional clean up 
-% clear ebsd_o opt_o ebsd1 MSdata2  
-% clear fullfile1 fullfile2 location_mtex
 
 %% Plot data
 setMTEXpref('xAxisDirection','east');           %aztec
